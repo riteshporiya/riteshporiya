89f5feedbad124e34594839398ba456b0c1309be
riteshporiya <riteshporiya124@gmail.com>
2023-11-19 13:29:31 +0530

    feat:added html template and header component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '**', pathMatch: 'full', redirectTo: '/'},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [ RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Meta, Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit{
+  title = 'riteshporiya-portfolio';
+
+  constructor(
+    private titleService: Title,
+    private metaService: Meta,
+  ){}
+
+  ngOnInit(): void {
+    this.titleService.setTitle( "Ritesh Poriya | MEAN Stack Developer" );
+
+    this.metaService.addTags([
+      {name: 'keywords', content: 'Frontend, software, developer'},
+      {name: 'description', content: 'Con 4 años de experiencia desarrollando sistemas, interfaces, bots y soluciones tecnológicas  para hacer de la web un lugar mejor. En mi trabajo me gusta liderar, proponer y ejecutar ideas, escribir y refactorizar código limpio, reutilizable y escalable.'},
+    ]);
+  }
 }
